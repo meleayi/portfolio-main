2cb4c4cb5d2ea9456cb8fcd32d6dfa1d3f281474
Melese <meleayi2021@gmail.com>
2026-05-12 15:00:11 +0300

    solve deploymnet issue related to tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    tsconfigPaths(),
+  ],
 });
